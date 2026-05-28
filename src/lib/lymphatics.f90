efd5689f2dbc63d92a5feba194f8029ab5333782
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-05-28 20:39:08 +1200

    Fix bash/powershell syntax error?

--- src/lib/lymphatics.f90
+++ src/lib/lymphatics.f90
@@ -33,8 +33,8 @@ contains
 !!!#############################################################################
 
   subroutine alveolar_capillary_flux(ne,write_out)
-    !*alveolar_capillary_flux:* calculate fluid flux from blood to interstitium
     !DEC$ ATTRIBUTES DLLEXPORT,ALIAS:"SO_ALVEOLAR_CAPILLARY_FLUX" :: ALVEOLAR_CAPILLARY_FLUX
+    !*alveolar_capillary_flux:* calculate fluid flux from blood to interstitium
 
     use other_consts,only: pi
 
