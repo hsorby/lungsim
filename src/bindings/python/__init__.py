cd46b1c4df9fc7873e59c08b5db3d1fc7f8df51d
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-05-29 21:32:35 +1200

    Fix bash/powershell syntax error?

--- src/bindings/python/__init__.py
+++ src/bindings/python/__init__.py
@@ -8,4 +8,6 @@ if platform.system() == "Windows":
     module_dir = os.path.dirname(__file__)
     os.add_dll_directory(module_dir)
     print(sys.prefix)
+    intel_library_path = os.path.join(sys.prefix, "Library", "bin")
+    print("Adding Intel library path:", intel_library_path)
     os.add_dll_directory("C:/hostedtoolcache/windows/Python/3.10.11/x64/Library/bin/")
