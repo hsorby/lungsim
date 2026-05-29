8205004a342d22bc48dbd0798d48eff6e6e73e5f
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-05-29 17:51:36 +1200

    Fix bash/powershell syntax error?

--- src/bindings/python/__init__.py
+++ src/bindings/python/__init__.py
@@ -1,8 +1,20 @@
 import os
 import platform
-import sys
-
+import site
 
 if platform.system() == "Windows":
-    intel_library_path = os.path.join(sys.prefix, "Library", "bin")
-    os.add_dll_directory(intel_library_path)
+    # Look through all site-packages dirs
+    for base in site.getsitepackages() + [site.getusersitepackages()]:
+        if not base:
+            continue
+
+        for pkg in (
+            "intel_fortran_rt",
+            "intel_openmp",
+            "intel_cmplr_lib_rt",
+            "intel_cmplr_lic_rt",
+            "impi_rt",
+        ):
+            candidate = os.path.join(base, pkg, "bin")
+            if os.path.isdir(candidate):
+                os.add_dll_directory(candidate)
