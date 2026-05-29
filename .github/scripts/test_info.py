e8e1232e5fa94b1e363f6069dc8c22758e87be83
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-05-29 19:19:19 +1200

    Fix bash/powershell syntax error?

--- .github/scripts/test_info.py
+++ .github/scripts/test_info.py
@@ -1,12 +1,23 @@
 
 import os
 
-import intel_fortran_rt
-print(os.path.dirname(intel_fortran_rt.__file__))
-import intel_openmp
-print(os.path.dirname(intel_openmp.__file__))
-import intel_cmplr_lib_rt
-print(os.path.dirname(intel_cmplr_lib_rt.__file__))
+try:
+  import intel_fortran_rt
+  print(os.path.dirname(intel_fortran_rt.__file__))
+except ModuleNotFoundError as e:
+  print("intel_fortran_rt not found:", e)
+
+try
+  import intel_openmp
+  print(os.path.dirname(intel_openmp.__file__))
+except ModuleNotFoundError as e:
+  print("intel_openmp not found:", e)
+
+try:
+  import intel_cmplr_lib_rt
+  print(os.path.dirname(intel_cmplr_lib_rt.__file__))
+except ModuleNotFoundError as e:
+  print("intel_cmplr_lib_rt not found:", e)
 
 
 print("DLL search dirs:")
